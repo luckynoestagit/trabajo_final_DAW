5489ad695dd56fbe4222db03de81cb87b32518a2
Lucas L. de Oliveira <luc14bhz@hotmail.com>
2026-05-14 22:25:05 +0200

    Limpieza raiz - eliminados duplicados del merge

--- menu/admin/__init__.py
+++
@@ -1,8 +0,0 @@
-from .productos_admin import ProductoAdmin
-from .cliente_admin import ClienteAdmin
-
-
-#añadir todo lo que necesita init
-
-__all__ = ['ProductoAdmin', 'ClienteAdmin']
-
--- menu/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .productos_model import Producto
-from .cliente_model import Cliente
-
-
-__all__ = ['Producto', 'Cliente']
